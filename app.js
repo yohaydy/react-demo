167d8b18e3b8b892503d3b68b49fd6a68e65a840
yohaydy <oskars.rukmans@gmail.com>
2016-09-17 21:54:50 +0300

    CommentVoter updates global application state

--- app.js
+++ app.js
@@ -1,32 +1,191 @@
-console.log("GUJ");
-var Comments = React.createClass({
-    render: function() {
-        return(
-            <div className="comments">
-                <Comment text="This is 1st Comment" rating={15}/>
-                <Comment text="This is 2nd Comment" rating={25}/>
-            </div>
-        )
-    }
+console.log("GdJ");
+
+
+var CommentBox = React.createClass({
+	getInitialState: function(){
+		return {data:[]};
+	},
+
+	loadCommentsFromServer: function(){
+		$.ajax({
+		url: this.props.url,
+		dataType: 'json',
+		cache: false,
+		success: function(data) {
+			this.setState({data: data});
+		}.bind(this),
+		error: function(xhr, status, err) {
+			console.error(this.props.url, status, err.toString());
+		}.bind(this)
+		});
+	},
+	
+	componentDidMount: function() {
+		this.loadCommentsFromServer();
+		setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+	},
+
+	handleCommentSubmit: function(comment) {
+		$.ajax({
+		url: this.props.url,
+		dataType: 'json',
+		type: 'POST',
+		data: comment,
+		success: function(data) {
+			this.setState({data: data});
+		}.bind(this),
+		error: function(xhr, status, err) {
+			console.error(this.props.url, status, err.toString());
+		}.bind(this)
+		});
+  	},	
+
+	handleCommentUpdate: function(comment) {
+		//TODO: post data to api
+		this.setState({data: data})
+	},
+
+	handleCommentVote: function(id, val){
+		console.log("Hi there " + val + " " + id);
+	},
+
+	render: function() {
+		return(
+			<div className="comment-box">
+				<h1>Comments</h1>
+				<CommentList data={this.state.data} handleCommentVote={this.handleCommentVote}/>
+				<CommentForm onCommentSubmit={this.handleCommentSubmit}/>
+			</div>
+			
+		)
+	}
 });
 
-var Comment = React.createClass({
-    render: function(){
-        return (
-            <div className="comment">
-                    <div className="comment-header">
-                        <div className="rating">{this.props.rating}</div>
-                        <button className="btn-vote">+</button>
-                        <button className="btn-vote">-</button>
-                    </div>
-                    <div className="comment-text">{this.props.text} </div>
-                </div>
-        )
-    }
+
+
+var CommentList = React.createClass({
+	propTypes: {
+		data: React.PropTypes.arrayOf(React.PropTypes.object)
+	},
+	render: function() {
+		var commentNodes = this.props.data.map((com)=> (
+			<Comment 
+			author={com.author} 
+			key={com.id} 
+			rating ={com.rating} 
+			text={com.text}
+			handleCommentVote={this.props.handleCommentVote}
+			id={com.id}
+			/>
+		))
+		return(
+			<div>
+				{commentNodes}
+			</div>
+		)
+	}
 })
 
+var Comment = React.createClass({
+
+	handleCommentVote: function(voteValue){
+		this.props.handleCommentVote(this.props.id, voteValue)
+	},
+
+	render: function(){
+		return (
+			<div className="comment">
+					<div className="comment-header">
+					  <div className="author">{this.props.author}</div>
+						<CommentVoter initialRating={this.props.rating} handleCommentVote={this.handleCommentVote}/>
+					</div>
+					<div className="comment-text">{this.props.text} </div>
+				</div>
+		)
+	}
+});
+
+var CommentVoter = React.createClass({
+
+	propTypes: {
+		data: React.PropTypes.number
+	},
+
+	getInitialState: function(){
+		return {rating: this.props.initialRating };
+	},
+	
+	voteUp: function(){
+		console.log(this.props)
+		this.props.handleCommentVote(1);
+	},
+	voteDown: function(){
+		this.props.handleCommentVote(-1);
+	},
+	
+	render: function(){
+		return(
+			<div>
+				<div className="rating">{this.state.rating}</div>
+				<button value={1} className="btn-vote" onClick={this.voteUp}>+</button>
+				<button value={-1} className="btn-vote" onClick={this.voteDown}>-</button>
+			</div>
+		)
+	}
+})
+
+var CommentForm = React.createClass({
+	getInitialState: function() {
+		return {author: '', text: ''};
+	},
+
+	handleAuthorChange: function(e) {
+		this.setState({author: e.target.value});
+	},
+
+	handleTextChange: function(e) {
+		this.setState({text: e.target.value});
+	},  
+
+	handleSubmit: function(e) {
+    e.preventDefault();
+    var author = this.state.author.trim();
+    var text = this.state.text.trim();
+    if (!text || !author) {
+      return;
+    }
+    this.props.onCommentSubmit({author: author, text: text});
+    this.setState({author: '', text: ''});
+	},
+
+	render: function() {
+		return (
+			<form className="commentForm" onSubmit={this.handleSubmit}>
+				<input
+				type="text"
+				placeholder="Your name"
+				value={this.state.author}
+				onChange={this.handleAuthorChange}
+				/>
+				<input
+				type="text"
+				placeholder="Say something..."
+				value={this.state.text}
+				onChange={this.handleTextChange}
+				/>
+				<input type="submit" value="Post" />
+			</form>
+		);
+  }
+});
+
 ReactDOM.render(
-    <Comments />,
-    document.getElementById('content')
+	<CommentBox url="http://localhost:3000/api/comments" pollInterval={30000} />,
+	document.getElementById('content')
 );
+
+
+
+
+
 
